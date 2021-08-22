79f5189f1934ab43f128cf73a132cef55ca23616
bela333 <b3kstudio@gmail.com>
2021-08-22 11:38:03 +0200

    Output image as Base64

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import renderer
 import generator
-
+import io
+import base64
 
 from dotenv import load_dotenv, find_dotenv
 load_dotenv(find_dotenv())
@@ -9,3 +10,7 @@ text = generator.generate()
 image = renderer.render(text)
 
 image.save("image.png")
+mem = io.BytesIO()
+image.save(mem, format = "PNG")
+mem.seek(0)
+print(base64.b64encode(mem.read()))
